d09811d8402942b0e310b076d5a098a2eb75a8fe
Parves Mosarof <parvesmosarof2@gmail.com>
2025-07-20 04:15:51 +0600

    fix login responsive

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -53,121 +53,107 @@ const Login = () => {
   };
 
   return (
-    <div className="relative">
-      <img
-        src={signInIMG}
-        className="absolute w-[50rem] h-[50rem] top-0 right-0"
-        alt="sign-in-image"
-      />
-      <div className="absolute left-0 min-h-screen flex items-center container mx-auto">
-        <div className="bg-white mt-[1rem] ml-[15rem] p-8 py-20 rounded-2xl  w-full max-w-md min-h-[40rem] flex flex-col justify-center">
-          <h2 className="text-3xl font-bold text-orangeAction">Welcome Back</h2>
-          <p className="text-lg font-semibold mb-8 text-brandGray mt-2">
-            Enter your email and password to sign in
-          </p>
+<div className="relative">
+  {/* Image - hidden on mobile */}
+  <img
+    src={signInIMG}
+    className="hidden lg:block absolute w-[50rem] h-[50rem] top-0 right-0"
+    alt="sign-in-image"
+  />
+  
+  <div className="absolute left-0 min-h-screen flex items-center container mx-auto px-4 lg:px-0">
+    <div className="bg-white mt-[1rem] lg:ml-[15rem] p-8 py-12 lg:py-20 rounded-2xl w-full max-w-md min-h-[40rem] flex flex-col justify-center">
+      <h2 className="text-3xl font-bold text-orangeAction">Welcome Back</h2>
+      <p className="text-lg font-semibold mb-8 text-brandGray mt-2">
+        Enter your email and password to sign in
+      </p>
 
-          <form onSubmit={handleLogin} className="space-y-6">
-            <div>
-              <label className="block text-sm font-medium text-darkGray">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                // required
-                ref={emailRef}
-                placeholder="Your email address"
-                className="mt-1 w-full px-4 py-4 border text-brandGray rounded-3xl focus:outline-none focus:ring-2 focus:ring-orange-500"
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-darkGray">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                // required
-                placeholder="Your password"
-                className="mt-1 w-full px-4 py-4 border text-brandGray rounded-3xl focus:outline-none focus:ring-2 focus:ring-orange-500"
-              />
-              <div className="flex justify-between items-center my-5 mt-8 text-sm">
-                <label className="flex items-center cursor-pointer">
-                  {/* Toggle Switch */}
-                  <div className="relative">
-                    <input type="checkbox" className="sr-only peer" />
-                    <div className="w-10 h-4 bg-gray-300 rounded-full peer-checked:bg-orange-500 transition" />
-                    <div className="absolute w-6 h-6 bg-white rounded-full -top-1 left-0 peer-checked:translate-x-full transition shadow" />
-                  </div>
-                  <span className="ml-3">Remember me</span>
-                </label>
-                <span
-                  onClick={handleForgotPassword}
-                  className="cursor-pointer hover:underline hidden"
-                >
-                  Forgot password?
-                </span>
+      <form onSubmit={handleLogin} className="space-y-6">
+        <div>
+          <label className="block text-sm font-medium text-darkGray">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            ref={emailRef}
+            placeholder="Your email address"
+            className="mt-1 w-full px-4 py-4 border text-brandGray rounded-3xl focus:outline-none focus:ring-2 focus:ring-orange-500"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-darkGray">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            className="mt-1 w-full px-4 py-4 border text-brandGray rounded-3xl focus:outline-none focus:ring-2 focus:ring-orange-500"
+          />
+          <div className="flex justify-between items-center my-5 mt-8 text-sm">
+            <label className="flex items-center cursor-pointer">
+              {/* Toggle Switch */}
+              <div className="relative">
+                <input type="checkbox" className="sr-only peer" />
+                <div className="w-10 h-4 bg-gray-300 rounded-full peer-checked:bg-orange-500 transition" />
+                <div className="absolute w-6 h-6 bg-white rounded-full -top-1 left-0 peer-checked:translate-x-full transition shadow" />
               </div>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-orangeAction hover:bg-orange-500 text-white font-semibold py-4 rounded-2xl transition"
+              <span className="ml-3">Remember me</span>
+            </label>
+            <span
+              onClick={handleForgotPassword}
+              className="cursor-pointer hover:underline hidden"
             >
-              Sign In
-            </button>
-          </form>
+              Forgot password?
+            </span>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-orangeAction hover:bg-orange-500 text-white font-semibold py-4 rounded-2xl transition"
+        >
+          Sign In
+        </button>
+      </form>
 
-          {error && (
-            <p className="text-red-600 text-center text-sm mt-3">{error}</p>
-          )}
+      {error && (
+        <p className="text-red-600 text-center text-sm mt-3">{error}</p>
+      )}
 
-          <p className="text-lg font-semibold text-brandGray text-center mt-4 mb-3 ">
-            Do not have an account?{" "}
-            <Link
-              to="/register"
-              className="text-orangeAction font-semibold hover:underline"
-            >
-              Sign up
-            </Link>
-          </p>
+      <p className="text-lg font-semibold text-brandGray text-center mt-4 mb-3">
+        Do not have an account?{" "}
+        <Link
+          to="/register"
+          className="text-orangeAction font-semibold hover:underline"
+        >
+          Sign up
+        </Link>
+      </p>
 
-          
-{/* OR Divider */}
-<div className="flex items-center my-4">
-  <div className="flex-grow h-px bg-gray-200" />
-  <span className="px-4 text-sm text-brandGray font-semibold">or, login with</span>
-  <div className="flex-grow h-px bg-gray-200" />
-</div>
+      {/* OR Divider */}
+      <div className="flex items-center my-4">
+        <div className="flex-grow h-px bg-gray-200" />
+        <span className="px-4 text-sm text-brandGray font-semibold">or, login with</span>
+        <div className="flex-grow h-px bg-gray-200" />
+      </div>
 
-
-          {/* Social Login Section */}
-          <div className="bg-white rounded-lg   mb-6 ">
-            
-
-
-            <div className="flex justify-center gap-4">
-              {/* <div className="border p-7 rounded-2xl cursor-pointer hover:bg-grayLightBg">
-                <img
-                  className="w-10 h-10 flex items-center justify-center  transition-colors"
-                  onClick={() => handleSocialLogin("Facebook")}
-                  src={FBLogo}
-                  alt="FBLogo"
-                />
-              </div> */}
-
-              <div className="border  rounded-2xl cursor-pointer hover:bg-grayLightBg">
-                <img
-                  className="w-full  flex items-center justify-center  transition-colors "
-                  onClick={() => handleSocialLogin("Google")}
-                  src={ContinueGoogle}
-                  alt="GoogleLogo"
-                />
-              </div>
-            </div>
+      {/* Social Login Section */}
+      <div className="bg-white rounded-lg mb-6">
+        <div className="flex justify-center gap-4">
+          <div className="border rounded-2xl cursor-pointer hover:bg-grayLightBg">
+            <img
+              className="w-full flex items-center justify-center transition-colors"
+              onClick={() => handleSocialLogin("Google")}
+              src={ContinueGoogle}
+              alt="GoogleLogo"
+            />
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
   );
 };
 
