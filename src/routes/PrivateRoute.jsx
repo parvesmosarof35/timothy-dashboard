a4043d08e86469869d7f8014a2fda4f07ebde0d0
parvesmosarof35 <parvesmosarof35@gmail.com>
2026-03-23 07:24:50 +0600

    fix dependencys

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Navigate } from "react-router";
+import { Navigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { jwtDecode } from "jwt-decode";
 import Swal from "sweetalert2";
